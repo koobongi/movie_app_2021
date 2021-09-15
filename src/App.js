058eb2bd42a1c5f27e97b0f6542f29872212c765
구본기 <201540202@daelim.ac.kr>
2021-09-15 15:28:43 +0900

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+
+const foodLik = [
+  {
+    name: "chikin",
+    image: "http://economychosun.com/query/upload/303/20190608214338_gubchoja.jpg"
+  },
+  {
+    name: "ham",
+    image: "https://img.newspim.com/news/2021/05/25/2105251548352120_w.jpg"
+  }
+]
+
 function App() {
   return (
     <div>
-    Hello React
+    <h1>Hello</h1>
+    <Food fav="kimchi" />
     </div>
-  );
+  )
 }
 
-export default App;
+function Food(foo) {
+  const { fav } = foo
+  return <h1>I like {fav}</h1>
+}
+export default App
