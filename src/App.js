2e3ef636414a96d647f03e10a2ea6612f930c59e
구본기 <201540202@daelim.ac.kr>
2021-09-29 16:45:48 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import {Component} from 'react'
 
-class App extends React.Component {
+class App extends Component {
+    constructor(props) {
+        super(props)
+        console.log('constructor')
+    }
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+    }
+    state = {
+        count: 0
+    }
+
+    add = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    minus = () => {
+        this.setState({count: this.state.count - 1})
+    }
+
     render() {
+        console.log('render')
         return (
-            <h1>Hello</h1>
+            <div>
+            <h1>Rhe number is: {this.state.count} </h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+            </div>
         )
     }
 }
