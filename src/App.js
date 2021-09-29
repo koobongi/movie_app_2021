e87e08ddd1e3db6f55f85c0b53b81da76ac97519
구본기 <201540202@daelim.ac.kr>
2021-09-29 15:40:57 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils"
+import React from 'react'
 
-
-const foodLike = [
-  {
-    name: "chikin",
-    image: "http://economychosun.com/query/upload/303/20190608214338_gubchoja.jpg"
-  },
-  {
-    name: "ham",
-    image: "https://img.newspim.com/news/2021/05/25/2105251548352120_w.jpg"
-  }
-]
-
-
-function App() {
-  return (
-    <div>
-      {
-        foodLike.map(dish => (<Food name={dish.name} picture={dish.image} />))
-      }
-    </div>
-  )
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
 }
 
-function Food({name, picture}) {
-  return (
-  <div>
-    <h1>I like {name} </h1>
-    <img src={picture} />
-  </div>
-  )
-}
 export default App
