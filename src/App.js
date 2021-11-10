16d45db876c3e8ede45ba5ba305eda7d1d8da215
구본기 <201540202@daelim.ac.kr>
2021-11-10 15:27:57 +0900

    ㅊㄱ

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
-import { HashRouter, Route } from 'react-router-dom'
+import "./App.css"
+import { HashRouter , Route} from 'react-router-dom'
 import About from './routes/About'
 import Home from "./routes/Home"
+import Navigation from "./components/Navigation"
+import Detail from "./routes/Detail"
 
-function App() {
-    return (
+function App(){
+    return(
         <HashRouter>
-              <Route path='/' exact={true} component={Home} />
+            <Navigation />
+            <Route path='/' exact={true} component={Home} />
             <Route path='/about' component={About} />
+            <Route path='/movie-detail' component={Detail} />
         </HashRouter>
     )
 }
 
-export default App
+export default App;
